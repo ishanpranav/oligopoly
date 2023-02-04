8f70ee89db86276a234c494a69d6e65f5eff7eb4
Ishan Pranav <ishan.pranav@outlook.com>
2023-02-04 01:02:47 -0500

    Add utility card

--- src/Oligopoly/Cards/ICard.cs
+++ src/Oligopoly/Cards/ICard.cs
@@ -9,6 +9,7 @@ namespace Oligopoly.Cards;
 [JsonDerivedType(typeof(AdvanceCard), "advance")]
 [JsonDerivedType(typeof(TravelCard), "travel")]
 [JsonDerivedType(typeof(RepairCard), "repair")]
+[JsonDerivedType(typeof(UtilityCard), "utility")]
 [JsonDerivedType(typeof(TaxCard), "tax")]
 [Union(0, typeof(JailbreakCard))]
 [Union(1, typeof(PoliceCard))]
@@ -16,7 +17,8 @@ namespace Oligopoly.Cards;
 [Union(3, typeof(AdvanceCard))]
 [Union(4, typeof(TravelCard))]
 [Union(5, typeof(RepairCard))]
-[Union(6, typeof(TaxCard))]
+[Union(6, typeof(UtilityCard))]
+[Union(7, typeof(TaxCard))]
 public interface ICard
 {
     CardId Id { get; set; }
