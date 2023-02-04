fd5e260f35b40a3ab678bae720e2e1fe7cb3bc7c
Ishan Pranav <ishan.pranav@outlook.com>
2023-02-03 19:00:09 -0500

    Add bonus card

---
+++ src/Oligopoly/Cards/BonusCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MessagePack;
+
+namespace Oligopoly.Cards;
+
+[MessagePackObject]
+public class BonusCard : ICard
+{
+    public BonusCard(string name, int amount)
+    {
+        ArgumentNullException.ThrowIfNull(name);
+
+        if (amount < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount));
+        }
+
+        Name = name;
+        Amount = amount;
+    }
+
+    /// <inheritdoc/>
+    [IgnoreMember]
+    public CardId Id { get; set; }
+
+    /// <inheritdoc/>
+    [Key(0)]
+    public string Name { get; }
+
+    [Key(1)]
+    public int Amount { get; }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return Name;
+    }
+}
--- src/Oligopoly/Cards/ICard.cs
+++ src/Oligopoly/Cards/ICard.cs
@@ -6,11 +6,13 @@ namespace Oligopoly.Cards;
 [JsonDerivedType(typeof(JailEscapeCard), "jailEscape")]
 [JsonDerivedType(typeof(PoliceCard), "police")]
 [JsonDerivedType(typeof(AdvanceCard), "advance")]
+[JsonDerivedType(typeof(BonusCard), "bonus")]
 [JsonDerivedType(typeof(RepairCard), "repair")]
 [Union(0, typeof(JailEscapeCard))]
 [Union(1, typeof(PoliceCard))]
 [Union(2, typeof(AdvanceCard))]
-[Union(3, typeof(RepairCard))]
+[Union(3, typeof(BonusCard))]
+[Union(4, typeof(RepairCard))]
 public interface ICard
 {
     CardId Id { get; set; }
